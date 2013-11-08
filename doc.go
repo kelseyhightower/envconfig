a0f5320c923842eb5cf5d54a277eae444c01ccf2
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-11-08 07:29:27 -0800

    Add package docs

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2013 Kelsey Hightower. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package envconfig implements decoding of environment variables based on a user
+// defined specification. A typical use is using environment variables for
+// configuration settings.
+package envconfig
