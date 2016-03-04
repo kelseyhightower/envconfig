c153e3f3dc961c733c8c2fff6c0f376702b6a60b
Travis J Parker <travis.parker@gmail.com>
2016-03-04 09:23:37 -0800

    cleanup pass, simplifying field handling funcs

--- envconfig.go
+++ envconfig.go
@@ -39,59 +39,54 @@ func Process(prefix string, spec interface{}) error {
 	typeOfSpec := s.Type()
 	for i := 0; i < s.NumField(); i++ {
 		f := s.Field(i)
-		if f.CanSet() {
-			if typeOfSpec.Field(i).Anonymous && f.Kind() == reflect.Struct {
-				embeddedPtr := f.Addr().Interface()
-				if err := Process(prefix, embeddedPtr); err != nil {
-					return err
-				}
-				f.Set(reflect.ValueOf(embeddedPtr).Elem())
-			}
+		if !f.CanSet() {
+			continue
+		}
 
-			alt := typeOfSpec.Field(i).Tag.Get("envconfig")
-			fieldName := typeOfSpec.Field(i).Name
-			if alt != "" {
-				fieldName = alt
+		if typeOfSpec.Field(i).Anonymous && f.Kind() == reflect.Struct {
+			embeddedPtr := f.Addr().Interface()
+			if err := Process(prefix, embeddedPtr); err != nil {
+				return err
 			}
-			key := strings.ToUpper(fmt.Sprintf("%s_%s", prefix, fieldName))
-			// `os.Getenv` cannot differentiate between an explicitly set empty value
-			// and an unset value. `os.LookupEnv` is preferred to `syscall.Getenv`,
-			// but it is only available in go1.5 or newer.
-			value, ok := syscall.Getenv(key)
-			if !ok && alt != "" {
-				key := strings.ToUpper(fieldName)
-				value, ok = syscall.Getenv(key)
-			}
+			f.Set(reflect.ValueOf(embeddedPtr).Elem())
+		}
 
-			def := typeOfSpec.Field(i).Tag.Get("default")
-			if def != "" && !ok {
-				value = def
-			}
+		alt := typeOfSpec.Field(i).Tag.Get("envconfig")
+		fieldName := typeOfSpec.Field(i).Name
+		if alt != "" {
+			fieldName = alt
+		}
+		key := strings.ToUpper(fmt.Sprintf("%s_%s", prefix, fieldName))
+		// `os.Getenv` cannot differentiate between an explicitly set empty value
+		// and an unset value. `os.LookupEnv` is preferred to `syscall.Getenv`,
+		// but it is only available in go1.5 or newer.
+		value, ok := syscall.Getenv(key)
+		if !ok && alt != "" {
+			key := strings.ToUpper(fieldName)
+			value, ok = syscall.Getenv(key)
+		}
 
-			req := typeOfSpec.Field(i).Tag.Get("required")
-			if !ok && def == "" {
-				if req == "true" {
-					return fmt.Errorf("required key %s missing value", key)
-				}
-				continue
-			}
+		def := typeOfSpec.Field(i).Tag.Get("default")
+		if def != "" && !ok {
+			value = def
+		}
 
-			parser := getParser(f.Type(), f.Kind())
-			if parser == nil {
-				continue
+		req := typeOfSpec.Field(i).Tag.Get("required")
+		if !ok && def == "" {
+			if req == "true" {
+				return fmt.Errorf("required key %s missing value", key)
 			}
+			continue
+		}
 
-			parsedValue, err := parser.Parse(value)
-			if err != nil {
-				return &ParseError{
-					KeyName:   key,
-					FieldName: fieldName,
-					TypeName:  f.Type().String(),
-					Value:     value,
-				}
+		err := processField(value, f)
+		if err != nil {
+			return &ParseError{
+				KeyName:   key,
+				FieldName: fieldName,
+				TypeName:  f.Type().String(),
+				Value:     value,
 			}
-			parser.Set(&f, parsedValue)
-
 		}
 	}
 	return nil
@@ -104,115 +99,58 @@ func MustProcess(prefix string, spec interface{}) {
 	}
 }
 
-// fieldParser represents a parsing function (conversion from string) and a
-// setting function (to set the value using reflection)
-type fieldParser struct {
-	Parse func(v string) (interface{}, error)
-	Set   func(f *reflect.Value, v interface{})
-}
+func processField(value string, field reflect.Value) error {
+	typ := field.Type()
 
-// getParser returns back a FieldParser instance for the given type
-func getParser(t reflect.Type, k reflect.Kind) (parser *fieldParser) {
-	switch k {
+	switch typ.Kind() {
 	case reflect.String:
-		parser = &fieldParser{
-			Parse: func(v string) (interface{}, error) {
-				return v, nil
-			},
-			Set: func(f *reflect.Value, v interface{}) {
-				f.SetString(v.(string))
-			},
-		}
+		field.SetString(value)
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		parser = &fieldParser{
-			Parse: func(v string) (interface{}, error) {
-				var intValue int64
-				var err error
-				if k == reflect.Int64 && t.PkgPath() == "time" && t.Name() == "Duration" {
-					var d time.Duration
-					d, err = time.ParseDuration(v)
-					intValue = int64(d)
-				} else {
-					intValue, err = strconv.ParseInt(v, 0, t.Bits())
-				}
-				if err != nil {
-					return nil, err
-				}
-
-				return intValue, nil
-			},
-			Set: func(f *reflect.Value, v interface{}) {
-				f.SetInt(v.(int64))
-			},
+		var (
+			val int64
+			err error
+		)
+		if field.Kind() == reflect.Int64 && typ.PkgPath() == "time" && typ.Name() == "Duration" {
+			var d time.Duration
+			d, err = time.ParseDuration(value)
+			val = int64(d)
+		} else {
+			val, err = strconv.ParseInt(value, 0, typ.Bits())
 		}
+		if err != nil {
+			return err
+		}
+
+		field.SetInt(val)
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		parser = &fieldParser{
-			Parse: func(v string) (interface{}, error) {
-				uintValue, err := strconv.ParseUint(v, 0, t.Bits())
-				if err != nil {
-					return nil, err
-				}
-
-				return uintValue, nil
-
-			},
-			Set: func(f *reflect.Value, v interface{}) {
-				f.SetUint(v.(uint64))
-			},
+		val, err := strconv.ParseUint(value, 0, typ.Bits())
+		if err != nil {
+			return err
 		}
-
+		field.SetUint(val)
 	case reflect.Bool:
-		parser = &fieldParser{
-			Parse: func(v string) (interface{}, error) {
-				boolValue, err := strconv.ParseBool(v)
-				if err != nil {
-					return nil, err
-				}
-				return boolValue, nil
-			},
-			Set: func(f *reflect.Value, v interface{}) {
-				f.SetBool(v.(bool))
-			},
+		val, err := strconv.ParseBool(value)
+		if err != nil {
+			return err
 		}
-
+		field.SetBool(val)
 	case reflect.Float32, reflect.Float64:
-		parser = &fieldParser{
-			Parse: func(v string) (interface{}, error) {
-				floatValue, err := strconv.ParseFloat(v, t.Bits())
-				if err != nil {
-					return nil, err
-				}
-				return floatValue, nil
-			},
-			Set: func(f *reflect.Value, v interface{}) {
-				f.SetFloat(v.(float64))
-			},
+		val, err := strconv.ParseFloat(value, typ.Bits())
+		if err != nil {
+			return err
 		}
+		field.SetFloat(val)
 	case reflect.Slice:
-		parser = &fieldParser{
-			Parse: func(v string) (interface{}, error) {
-				elemType := t.Elem()
-				parser := getParser(elemType, elemType.Kind())
-				strValues := strings.Split(v, ",")
-
-				slice := reflect.MakeSlice(reflect.SliceOf(elemType), 0, 0)
-				for i := range strValues {
-					value, err := parser.Parse(strValues[i])
-					if err != nil {
-						return nil, err
-					}
-
-					itmValue := reflect.ValueOf(value)
-					itmValue = itmValue.Convert(elemType)
-
-					slice = reflect.Append(slice, itmValue)
-				}
-				return slice.Interface(), nil
-			},
-			Set: func(f *reflect.Value, v interface{}) {
-				f.Set(reflect.ValueOf(v))
-			},
+		vals := strings.Split(value, ",")
+		sl := reflect.MakeSlice(typ, len(vals), len(vals))
+		for i, val := range vals {
+			err := processField(val, sl.Index(i))
+			if err != nil {
+				return err
+			}
 		}
+		field.Set(sl)
 	}
-	return
+
+	return nil
 }
