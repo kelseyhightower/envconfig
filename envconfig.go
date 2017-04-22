7e2d3ab36111c1ccaa34a3cb7623b8c255da2d67
dmiles <dmiles@monsooncommerce.com>
2017-04-22 13:07:23 -0700

    adding a method to process variables from etcd with the attending test case, and a refactor so the new test case can use code from the existing TestProcess test case

--- envconfig.go
+++ envconfig.go
@@ -5,6 +5,7 @@
 package envconfig
 
 import (
+	"context"
 	"encoding"
 	"errors"
 	"fmt"
@@ -13,6 +14,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/coreos/etcd/client"
 )
 
 // ErrInvalidSpecification indicates that a specification is of the wrong type.
@@ -183,6 +186,27 @@ func Process(prefix string, spec interface{}) error {
 	}
 
 	return err
+}
+
+// ProcessFromEtcd modifies lookupFunc so that values come from the etcd
+// KeysAPI passed in as an argument, then calls Process as per usual
+func ProcessFromEtcd(prefix string, spec interface{}, kapi client.KeysAPI) error {
+	type lookupEnvFunc func(key string) (value string, found bool)
+	defer func(oldLookup lookupEnvFunc) {
+		lookupEnv = oldLookup
+	}(lookupEnv)
+	lookupEnv = func(key string) (value string, found bool) {
+		key = "/" + key
+		result, err := kapi.Get(context.Background(), key, nil)
+		if err != nil {
+			return "", false
+		}
+		if result == nil || result.Node == nil {
+			return "", false
+		}
+		return result.Node.Value, true
+	}
+	return Process(prefix, spec)
 }
 
 // MustProcess is the same as Process but panics if an error occurs
