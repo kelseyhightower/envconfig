e035b04466511c9bb847a27be4c0684fb2dec1b7
David Terranova <david@smallpdf.com>
2020-08-16 10:04:47 +0200

    apply default if zero value only

--- envconfig_test.go
+++ envconfig_test.go
@@ -448,6 +448,17 @@ func TestAlternateNameDefaultVar(t *testing.T) {
 		t.Error(err.Error())
 	}
 
+	if s.NoPrefixDefault != "betterbroker" {
+		t.Errorf("expected %q, got %q", "betterbroker", s.NoPrefixDefault)
+	}
+
+	os.Clearenv()
+	s.NoPrefixDefault = ""
+	os.Setenv("ENV_CONFIG_REQUIREDVAR", "foo")
+	if err := Process("env_config", &s); err != nil {
+		t.Error(err.Error())
+	}
+
 	if s.NoPrefixDefault != "127.0.0.1" {
 		t.Errorf("expected %q, got %q", "127.0.0.1", s.NoPrefixDefault)
 	}
@@ -794,7 +805,7 @@ func TestCheckDisallowedIgnored(t *testing.T) {
 
 func TestErrorMessageForRequiredAltVar(t *testing.T) {
 	var s struct {
-		Foo    string `envconfig:"BAR" required:"true"`
+		Foo string `envconfig:"BAR" required:"true"`
 	}
 
 	os.Clearenv()
