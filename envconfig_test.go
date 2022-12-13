b2bc723bbd54f1c4b5617a93e3f1023e9f9eb034
Jeroen Demeyer <jeroen.demeyer@be-mobile.com>
2022-12-13 10:46:20 +0100

    Usage: support implementsInterface() for non-struct types

--- envconfig_test.go
+++ envconfig_test.go
@@ -64,12 +64,14 @@ type Specification struct {
 		Property            string `envconfig:"inner"`
 		PropertyWithDefault string `default:"fuzzybydefault"`
 	} `envconfig:"outer"`
-	AfterNested  string
-	DecodeStruct HonorDecodeInStruct `envconfig:"honor"`
-	Datetime     time.Time
-	MapField     map[string]string `default:"one:two,three:four"`
-	UrlValue     CustomURL
-	UrlPointer   *CustomURL
+	AfterNested      string
+	DecodeStruct     HonorDecodeInStruct `envconfig:"honor"`
+	Datetime         time.Time
+	MapField         map[string]string `default:"one:two,three:four"`
+	UrlValue         CustomURL
+	UrlPointer       *CustomURL
+	NamedSliceValue  NamedSlice
+	CustomSliceValue SliceWithUnmarshal
 }
 
 type Embedded struct {
@@ -84,6 +86,14 @@ type Embedded struct {
 type EmbeddedButIgnored struct {
 	FirstEmbeddedButIgnored  string
 	SecondEmbeddedButIgnored string
+}
+
+type NamedSlice []int8
+
+type SliceWithUnmarshal []int8
+
+func (cs *SliceWithUnmarshal) UnmarshalText(text []byte) error {
+	return nil
 }
 
 func TestProcess(t *testing.T) {
